da7fe8d5ff33cf1ce0004615d1b784a5806c7e8c
Adria Cano Quintana <canoquintanaadria@fpllefia.com>
2021-09-30 02:07:24 +0200

    Entrega práctica

---
+++ N2P1AdriaCano.php
@@ -0,0 +1,68 @@
+﻿
+<?php
+	session_start();
+	
+	if (!isset($_SESSION['logged']))
+		$_SESSION['logged'] = false;
+
+	if ($_SESSION['logged']) {		
+		header('Location: ./principal.php');
+		exit();
+	}
+		
+	if (isset($_POST['username']) && isset($_POST['recover']) && ($_POST['recover'] == 1))
+	{
+		$_SESSION['recovering'] = 1;
+	}
+	
+	if (isset($_POST['username']) && isset($_POST['userpassword']))
+	{
+		if ($_POST['username'] == 'admin' && $_POST['userpassword'] == '1234') 
+		{			
+			/* Simulamos que leemos de base de datos los datos del usuario */
+			
+			$_SESSION['name'] = 'Adrià';
+			$_SESSION['surname'] = 'Cano Quintana';
+			
+			$_SESSION['logged'] = true;
+		}
+		else {
+			$_SESSION['logged'] = false;
+		}
+		
+		header('Location: ./N2P1AdriaCano.php');
+	}
+?>
+
+<html>
+	<head>
+		<title> Transparent Login Form Design </title>
+		<link rel="stylesheet" type="text/css" href="style.css">   
+	</head>
+	<body>		
+		<div class="login-box">
+			<img src="avatar.png" class="avatar" alt="avatar">
+			<h1>Iniciar sesion</h1>
+			
+			<form action="./N2P1AdriaCano.php" method="post" id="loginform">
+				<p>Usuario</p>
+				<input type="text" name="username" placeholder="Usuario">
+				<p>Contraseña</p>
+				<input type="password" name="userpassword" placeholder="Contraseña">								
+				<input type="submit" value="login">
+				
+				<input hidden type="number" name="recover" value="0" />							
+
+				<?php 
+					if (isset($_SESSION['recovering']) && $_SESSION['recovering'] == 1) {
+						echo '<p>Solicitud de recuperación de clave enviada</p>';						
+					}
+					else {
+						echo '<a href="javascript:document.getElementsByName(' . "'recover'" . ')[0].value = 1; loginform.submit();">Recuperar Clave</a>';
+					}
+				?>				
+				
+			</form>               
+		</div>    
+	</body>
+</html>
---
+++ principal.php
@@ -0,0 +1,59 @@
+﻿
+<?php
+	session_start();
+	
+	if (!isset($_SESSION['logged']) || $_SESSION['logged'] == false)
+		header('Location: ./N2P1AdriaCano.php');
+?>
+
+<html>
+	<head>
+		<title> Mi web </title>
+		<link rel="stylesheet" type="text/css" href="style.css">   
+	</head>
+	<body>
+		<p>Hola <?php echo $_SESSION['name'] . ' ' . $_SESSION['surname'] ?>, <br />
+			Encantado de verte aquí otra vez
+		</p>		
+		
+		<p>Hora actual del servidor: <?php echo date(DATE_RFC2822); ?>.</p>
+		
+		<?php
+			 if (isset($_GET['area']))
+			 {
+				 setcookie('lastaction', $_GET['area'], 0);				 
+				 switch ($_GET['area'])					 
+				 {
+					 case 'compras':
+						echo '<p>Te encuentras en compras</p>';											
+						break;
+					 case 'catalogo':
+						echo '<p>Te encuentras en el catálogo de productos</p>';												
+						break;		
+					 default:
+						echo '<p>Error 404, la página no existe</p>';
+						break;
+				 }
+				 
+				 echo '<br />';
+				 echo '<a href="./principal.php">Volver a la página principal</a><br />';
+			 }
+			 else
+			 {
+				 /* Gracias a la cookie "lastaction", recordamos la última página que visitó el usuario y ¡le permitimos reanudar la visita por dónde lo dejó! */
+				 
+				 echo '<p>Estás en la página principal</p><br />';
+				 
+				 if (isset($_COOKIE['lastaction'])) {
+					echo '<a href="/principal.php?area=' . urlencode($_COOKIE['lastaction']) . '">Haz clic aquí para visitar la última página que visitase en la web</a><br />';
+				 }
+				 
+				 /* Si la palabra fuera por ejemplo un nombre de artículo, urlencode() sanearía espacios y caracteres extraños para pasarlos correctamente por GET. */
+				 
+				 echo '<a href="./principal.php?area=' . urlencode('compras') . '">Compras</a><br />';
+				 echo '<a href="./principal.php?area=' . urlencode('catalogo') . '">Catalogo</a><br />';
+			 }
+		?>
+		</p>		
+	</body>
+</html>
